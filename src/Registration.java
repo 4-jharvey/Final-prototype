3700642ec5036941a776be3a91034c49e2721a5e
4-JHarvey <4-JHarvey@BH-R20-19.local.gfmat.org>
2025-10-01 14:00:22 +0100

    This commit shows the start of my prototype in preparation for the NEA project

--- src/Registration.java
+++ src/Registration.java
@@ -119,9 +119,9 @@ public class Registration extends javax.swing.JFrame {
         String PlayerAmount = NumOfPlayers.getText();
         String Mail = Email.getText();
         
-        if(Name.equals(" ") && PlayerAmount.equals(" ") && Mail.equals(" ")){
+        if(Name.equals("") && PlayerAmount.equals("") && Mail.equals("")){
             
-            JOptionPane.showMessageDialog(null,"Please fill in the required parts of the form");
+               JOptionPane.showMessageDialog(null,"Please fill in the required parts of the form");
         }
         else{
             CompletionScreen Completion = new CompletionScreen();
